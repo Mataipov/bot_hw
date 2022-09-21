765389731ed216c5369e6845ecc625cc38c4ac80
Mataipov <111882291+Mataipov@users.noreply.github.com>
2022-09-21 17:59:33 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from aiogram.utils import executor
 from config import dp
 import logging
 from handlers import clients, callback, extra, admins, fsm_admin_menu
+from database.db import sql_create
+
+
+async def on_startup(_):
+    sql_create()
 
 clients.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
@@ -12,5 +17,5 @@ extra.register_handlers_extra(dp)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup))
 
