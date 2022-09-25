18bdca1f670ae067b087255b695c2c2402de39da
Mataipov <111882291+Mataipov@users.noreply.github.com>
2022-09-25 18:37:03 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+import asyncio
 from aiogram.utils import executor
 from config import dp
 import logging
-from handlers import clients, callback, extra, admins, fsm_admin_menu
+from handlers import clients, callback, extra, admins, fsm_admin_menu, notifications
 from database.db import sql_create
 
 
 async def on_startup(_):
+    asyncio.create_task(notifications.schedule())
     sql_create()
 
 clients.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
 fsm_admin_menu.register_handlers_fsm_menu(dp)
+notifications.register_handlers_notification(dp)
 
 extra.register_handlers_extra(dp)
 
